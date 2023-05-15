912aac28a3e4b6ca71b5275ed11d4b70ca164973
Mesut GEDİK <112901255+mesutgdk@users.noreply.github.com>
2023-05-15 11:31:49 +0300

    Add files via upload

--- TwitterMind/TwitterMind/ViewController.swift
+++ TwitterMind/TwitterMind/ViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import SwifteriOS
+import CoreML
 
 class ViewController: UIViewController {
 
@@ -14,13 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var faceLabel: UILabel!
     @IBOutlet weak var textField: UITextField!
     
+    let sentimentClassifier = TweetMindClassifier()
+    
     let swifter = Swifter(consumerKey: "Your App Key here", consumerSecret: "Your App Secret Here")
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        swifter.searchTweet(using: "@Apple",success: { (results, searchMetadata) in
+        let prediction = try! sentimentClassifier.prediction(text: "@Apple is a terrible compony")
+        
+        print(prediction.label)
+        
+        swifter.searchTweet(using: "@Apple", lang:"en", count:100, tweetMode: .extended ,success: { (results, searchMetadata) in
             print(results)
         }, failure: {error in
             print("there is an error with Twitter API Request, \(error)")
